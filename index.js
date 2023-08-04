6ef286755762e57c497b32941af8817b310a92d3
vinay <vinay@gmail.com>
2023-08-04 14:32:29 +0000

    express js introduction

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+app.get("/date", (request, response) => {
+  let date = new Date();
+  response.send(`Todays date is ${date}`);
+  console.log(date);
+});
+
+app.get("/page", (request, response) => {
+  response.sendFile("./page.html", { root: __dirname });
+});
+
+app.listen(3000);
